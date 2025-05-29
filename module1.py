806475296aa154e63bd45e60945abfba73df3a93
XeroHer <bksraut27@gmail.com>
2025-05-29 15:26:11 +0100

    module

---
+++ module1.py
@@ -0,0 +1,8 @@
+# method 1:   import module
+# method 2:import module  as object
+
+# method 3: from module import person
+# method 4: from module import person as object
+#a=module.person["age"] 
+# a = object.person
+print(a)  # Output: 30
